9ca2d9dba64064c395b4d1d24ee962d5c11b3b26
Anisha Agarwal <anishaagg@gmail.com>
2016-10-13 13:02:58 -0700

    Initial Commit

---
+++ PageTheScroll/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PageTheScroll
+//
+//  Created by Agarwal,Anisha on 10/13/16.
+//  Copyright © 2016 Agarwal,Anisha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
