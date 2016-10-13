19d670cd7521594dc404ee68f4830867d084364a
Anisha Agarwal <anishaagg@gmail.com>
2016-10-13 16:11:12 -0700

    Working scroll view

--- PageTheScroll/ViewController.swift
+++ PageTheScroll/ViewController.swift
@@ -9,10 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var images = [UIImageView]()
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        var contentWidth: CGFloat = 0.0
+        
+        for x in 0...2 {
+            let image = UIImage(named: "icon\(x).png")
+            let imageView = UIImageView(image: image)
+            images.append(imageView)
+            
+            let scrollWidth = scrollView.frame.size.width
+            var newX: CGFloat = 0.0
+            newX = scrollWidth/2 + scrollWidth * CGFloat(x)
+            
+            contentWidth += newX
+            
+            scrollView.addSubview(imageView)
+            imageView.frame = CGRect(x: newX - 75, y: (scrollView.frame.size.height/2) - 75, width: 150, height: 150)
+        }
+        
+        scrollView.contentSize = CGSize(width: contentWidth, height: view.frame.size.height)
+        scrollView.clipsToBounds = false
+
     }
 
     override func didReceiveMemoryWarning() {
